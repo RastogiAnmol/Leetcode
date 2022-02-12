1b2b8f1713810b9f3b52d379e02abe90049b1f35
Anmol <anmolamity@gmail.com>
2022-02-12 17:08:48 +0530

    init

---
+++ src/DummyClass.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DummyClass {
+    public static void main(String[] args) {
+        System.out.println("Anmol");
+    }
+}
+
