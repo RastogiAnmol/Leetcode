6d02f02df7fecdc5a7af649e53b4da643a6068c6
Anmol <anmolamity@gmail.com>
2022-03-05 17:19:25 +0530

    tree traverse

---
+++ src/SpiralTraverse.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralTraverse {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int up = 0;
+        int left = 0;
+        int right = columns - 1;
+        int down = rows - 1;
+
+        while (result.size() < rows * columns) {
+            // Traverse from left to right.
+            for (int col = left; col <= right; col++) {
+                result.add(matrix[up][col]);
+            }
+            // Traverse downwards.
+            for (int row = up + 1; row <= down; row++) {
+                result.add(matrix[row][right]);
+            }
+            // Make sure we are now on a different row.
+            if (up != down) {
+                // Traverse from right to left.
+                for (int col = right - 1; col >= left; col--) {
+                    result.add(matrix[down][col]);
+                }
+            }
+            // Make sure we are now on a different column.
+            if (left != right) {
+                // Traverse upwards.
+                for (int row = down - 1; row > up; row--) {
+                    result.add(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+        int flag=1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                    matrix[i][j] = flag++;
+            }
+        }
+        SpiralTraverse st = new SpiralTraverse();
+        st.spiralOrder(matrix);
+    }
+}
